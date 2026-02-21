d1a39c7d322068bd83bfb386a51babdb60fac885
wahaj248 <wahajkhan2488@gmail.com>
2026-02-21 06:48:08 +0500

    bridge add

--- src/app-bridge.js
+++ src/app-bridge.js
@@ -1,12 +1,13 @@
 import createApp from "@shopify/app-bridge";
 
+// Get URL parameters
 const params = new URLSearchParams(window.location.search);
 
 const app = createApp({
-  apiKey: "a2afe2f64c425f93f052bfaece617ca5",         // Shopify Partner Dashboard se
-  host: params.get("host") || "dummy_host",  // embedded app URL me milta hai
-  shop: params.get("shop") || "test-store.myshopify.com",
-  forceRedirect: true,                      // Shopify admin me redirect
+  apiKey: "a2afe2f64c425f93f052bfaece617ca5", // Shopify Partner Dashboard se
+  host: params.get("host"),   // Shopify admin iframe me automatically aayega
+  shop: params.get("shop"),   // Shopify store param
+  forceRedirect: true,        // Embedded app ke liye Shopify admin me redirect
 });
 
 export default app;
